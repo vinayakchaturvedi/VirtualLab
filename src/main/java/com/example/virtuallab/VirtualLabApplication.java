09c98eac39a234390c6bce0f6009ff54a5645352
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2021-03-30 17:39:15 +0530

    Creating Project template

---
+++ src/main/java/com/example/virtuallab/VirtualLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.virtuallab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualLabApplication.class, args);
+    }
+
+}
