173fa9392361c155caea93e83ab417cd84554903
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2021-05-12 19:59:21 +0530

    Code Cleanup

--- src/main/webapp/virtual-lab-ui/src/assets/jss/material-dashboard-react/components/rtlHeaderLinksStyle.js
+++ src/main/webapp/virtual-lab-ui/src/assets/jss/material-dashboard-react/components/rtlHeaderLinksStyle.js
@@ -2,9 +2,9 @@ import {
   defaultFont,
   dangerColor,
   whiteColor
-} from "assets/jss/material-dashboard-react.js";
+} from "../../material-dashboard-react.js";
 
-import dropdownStyle from "assets/jss/material-dashboard-react/dropdownStyle.js";
+import dropdownStyle from "../../material-dashboard-react/dropdownStyle.js";
 
 const headerLinksStyle = theme => ({
   ...dropdownStyle(theme),
