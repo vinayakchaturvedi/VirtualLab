356f2a7fa085fe9cf831b392f75972263989c55d
vinayak.chaturvedi96@gmail.com <vinayak.chaturvedi96@gmail.com>
2021-05-12 19:14:24 +0530

    Code Cleanup

--- src/main/java/com/example/virtuallab/VirtualLabApplication.java
+++ src/main/java/com/example/virtuallab/VirtualLabApplication.java
@@ -1,6 +1,5 @@
 package com.example.virtuallab;
 
-import com.example.virtuallab.service.ExecuteLinuxProcess;
 import com.example.virtuallab.service.SemesterEnd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,13 +12,6 @@ public class VirtualLabApplication {
         ConfigurableApplicationContext context = SpringApplication.run(VirtualLabApplication.class, args);
         SemesterEnd semesterEnd = context.getBean(SemesterEnd.class);
         semesterEnd.checkIfAnyLabNeedsToBeDestroyed();
-        startNpm();
-    }
-
-    private static void startNpm() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("npm", "start", "--prefix", System.getProperty("user.dir") + "/src/main/webapp/virtual-lab-ui/");
-        new ExecuteLinuxProcess().executeProcess(processBuilder);
     }
 
 }
